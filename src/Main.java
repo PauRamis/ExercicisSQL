b2f482931a164f5ddb85ba88d007260da5ca477f
Pau <pauramisnunez@gmail.com>
2023-06-01 19:16:19 +0200

    First query

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 //First, apply mysql-connector to project structure
 public class Main {
     private static String DB_USERNAME = "root";
-    private static String DB_PORT = ":8082";
     private static String DB_PASSWORD = "password";
+    private static String DB_PORT = ":3306";
     private static String DB_HOST = "localhost";
     private static String DB_DBNAME = "db";
+
     public static void main(String[] args) throws SQLException {
-        Connection con = DriverManager.getConnection(
-                "jdbc:mysql://"+DB_HOST+DB_PORT+"/"+DB_DBNAME,DB_USERNAME, DB_PASSWORD
-        );
-        String sql = "insert into test (text) values (?)";
+        String url = "jdbc:mysql://" + DB_HOST + DB_PORT + "/" + DB_DBNAME;
+        Connection con = DriverManager.getConnection(url, DB_USERNAME, DB_PASSWORD);
+        if (con != null) {
+            System.out.println("Connected to the database");
+        } else throw new RuntimeException("Err: Not connected to database");
+
+        String query = "Show tables";
+        PreparedStatement ps = con.prepareStatement(query);
+        ResultSet result = ps.executeQuery(query);
+        System.out.println("Tables in the current database: ");
+        while(result.next()) {
+            System.out.print(result.getString(1));
+            System.out.println();
+        }
+
+        /*String sql = "insert into test (text) values (?)";
         PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, "prova");
         ps.execute();
-        ps.close();
+        ps.close();*/
     }
 }
 /*
